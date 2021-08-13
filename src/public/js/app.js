4aa4de441f28065baa911c62a89a18c93b60ddc0
raejun92 <cvcv1324@naver.com>
2021-08-13 10:31:17 +0900

    ws설치 및 http와 sw 동시 사용하여 front와 back-end의 통신 과정

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-alert('hi')
+const socket = new WebSocket(`ws:${window.location.host}`); // socket은 서버로의 연결을 뜻함
+
+socket.addEventListener("open", () => {
+	console.log("Connected to Server");
+});
+
+socket.addEventListener("message", message => {
+	console.log("New message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+	console.log("Disconnected to Server");
+});
+
+setTimeout(() => {
+	socket.send("hello from the browser!");
+}, 5000);
--- src/server.js
+++ src/server.js
@@ -1,12 +1,28 @@
 import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-console.log("server");
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
+
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+// 이렇게 하면 http서버와 webSocket서버 둘 다 사용 가능
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server}); // http서버 위에 webSocket 서버를 만든다
+
+wss.on("connection", socket => { // socket은 연결된 브라우저를 뜻함
+	console.log("Connected to Browser");
+	socket.on("close", () => console.log("Disconnected from browser"));
+	socket.on("message", (message) => {
+		console.log(message.toString('utf8'));
+	});
+	socket.send("hello!");
+});
+
+server.listen(3000, handleListen);
