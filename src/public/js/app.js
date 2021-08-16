993219d569bcda3703fee90268a2b05dbf52787c
raejun92 <cvcv1324@naver.com>
2021-08-16 10:42:47 +0900

    다른 브라우저간 통신 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,24 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
 const socket = new WebSocket(`ws:${window.location.host}`); // socket은 서버로의 연결을 뜻함
 
-socket.addEventListener("open", () => {
+socket.addEventListener("open", () => { // 서버에 연결
 	console.log("Connected to Server");
 });
 
-socket.addEventListener("message", message => {
+socket.addEventListener("message", message => { // 서버로부터 메시지를 받음
 	console.log("New message: ", message.data);
 });
 
-socket.addEventListener("close", () => {
+socket.addEventListener("close", () => { // 서버가 연결을 종료
 	console.log("Disconnected to Server");
 });
 
-setTimeout(() => {
-	socket.send("hello from the browser!");
-}, 5000);
+function handleSubmit(event) {
+	event.preventDefault();
+	const input = messageForm.querySelector("input");
+	socket.send(input.value);
+	input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -16,13 +16,15 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server}); // http서버 위에 webSocket 서버를 만든다
 
+const sockets = [];
+
 wss.on("connection", socket => { // socket은 연결된 브라우저를 뜻함
+	sockets.push(socket);
 	console.log("Connected to Browser");
-	socket.on("close", () => console.log("Disconnected from browser"));
-	socket.on("message", (message) => {
-		console.log(message.toString('utf8'));
+	socket.on("close", () => console.log("Disconnected from browser")); // 브라우저가 연결을 끊음(창 닫기)
+	socket.on("message", (message) => { // 브라우저로부터 메시지를 받음
+		sockets.forEach(aSocket => aSocket.send(message.toString()));
 	});
-	socket.send("hello!");
 });
 
 server.listen(3000, handleListen);
