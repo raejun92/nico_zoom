50f8fed7d8ae55f6e60125edae283a02c298ec8f
raejun92 <cvcv1324@naver.com>
2021-08-22 16:06:12 +0900

    2.2 SocketIO로 front-end와 back-end 통신

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,15 @@
 const socket = io();
+
+const welcome = document.querySelector("#welcome");
+const form = welcome.querySelector("form");
+
+function handleRoomSubmit(event) {
+	event.preventDefault();
+	const input = form.querySelector("input");
+	socket.emit("enter_room", {payload: input.value}, () => {
+		console.log("Server is done!");
+	});
+	input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
